b55151c7a7cc4ca9c85a6afdcfc1d599082e0f45
jampibryan <jampibryan@gmail.com>
2022-08-03 11:34:45 -0500

    dark mode with user security

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="flex-shrink-0 px-6 py-4">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
+        <span>Made with</span>
+        
+        <span>by</span>
+        <a href="https://github.com/jampibryan" target="_blank" class="text-blue-600 hover:underline">
+            Jampi Bryan
+        </a>
+    </p>
+</footer>
